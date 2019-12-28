4c604906ca5a74e406a7e72cfd96be112ac38b9a
조경진 <kj_cho@idealidea.co.kr>
2019-12-28 16:43:51 +0900

    유져 목록 뿌리는 어댑터 초안 작성  - 복붙으로 쉽게 작성

---
+++ app/src/main/java/com/tjeit/a20191228_01_apipractice/adapters/UserAdapter.kt
@@ -0,0 +1,34 @@
+package com.tjeit.a20191228_01_apipractice.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.tjeit.a20191228_01_apipractice.R
+import com.tjeit.a20191228_01_apipractice.datas.User
+
+class UserAdapter(context:Context, resId:Int, list: ArrayList<User>) : ArrayAdapter<User>(context, resId, list) {
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.user_list_item, null)
+        }
+        val row = tempRow!!
+
+        val data = mList.get(position)
+
+
+
+        return row
+    }
+
+
+}
